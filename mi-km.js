3a06fc133aa51073a8c7e5462cbf5bb06c46421b
Max DeHerrera <maxdehe3@gmail.com>
2014-08-21 13:18:06 -0600

    mi-km.js

---
+++ mi-km.js
@@ -0,0 +1,37 @@
+/**
+ *takes miles and converts it to kilometers
+ *
+ *@param takes miles
+ *@returns convert miles to kilometers
+ **/
+function milesConvert(miles) {
+    // first, perform the task (add the numbers)
+    var distance = miles * 1.60934;
+    
+    //when the task is complete return the results
+    return (distance);
+}
+
+/**
+ *callback function converting miles to kilometers
+ **/
+function milesConvertCallback() {
+    //get inputs from the DOM
+    var distance = document.getElementById("miles").value;
+    
+    //sanitize inputs
+    distance = parseFloat(distance);
+    
+    
+    //verify inputs
+    if (isNaN(distance) === true) {
+        alert ("Invalid input!");
+        return;
+    }
+    
+    //use the worker function and get the output
+    var conversion = milesConvert(distance);
+    
+    //do something "useful" - i.e., display the answer
+    document.getElementById("outputArea").innerHTML = conversion + " km";
+}
