692158584fa59db983d792212a4dfed5636308d1
Jim wittwer <wittwerjim@gmail.com>
2014-08-21 16:00:31 -0600

    added celcius and fahrenheit function files

---
+++ temp-conversion_jim/temp-conversion_jim.js
@@ -0,0 +1,89 @@
+/**
+ * converts celsius to fahrenheit
+ *
+ * @param double celsius to be converted
+ * @returns double sum of celsius converted to fahrenheit
+ **/
+function tempConvertCel(celsius) {
+    // Multiply Celsius by 1.8 then add 32
+    var sum = (celsius * 1.8) + 32;
+    
+    // when the task is coplete return the reults
+    return(sum);
+}
+
+
+/**
+ * converts fahrenheit to celsius
+ *
+ * @param double fahrenheit to be converted
+ * @returns double sum of fahrenheit converted to celsius
+ **/
+function tempConvertFah(fahrenheit) {
+    // Subtract 32 from fahrenheit, then divide by 1.8
+    var sum = (fahrenheit - 32) / 1.8;
+    var sum = sum.toFixed(2);
+    
+    // when the task is coplete return the reults
+    return(sum);
+}
+
+
+/**
+ * callback function for celsius to fahrenheit
+ **/
+function tempConvertCelCallback() {
+    // get inputs from the DOM
+    var celsius = document.getElementById("celsius").value;
+    
+    // sanitize inputs
+    celsius  = parseFloat(celsius);
+    
+    // verify inputs
+    if(isNaN(celsius) === true) {
+        alert("Invalid temperature, try again");
+        return;
+    }
+    
+    // use the worker function and get the output
+    var sum = tempConvertCel(celsius);
+    
+    // do something "useful" - i.e., display the answer
+    document.getElementById("outputAreaFahrenheit").innerHTML = sum + " degrees Fahernheit";
+
+}
+
+
+/**
+ * callback function for fahrenheit to celsius
+ **/
+function tempConvertFahCallback() {
+    // get inputs from the DOM
+    var fahrenheit = document.getElementById("fahrenheit").value;
+    
+    // sanitize inputs
+    fahrenheit  = parseFloat(fahrenheit);
+    
+    // verify inputs
+    if(isNaN(fahrenheit) === true) {
+        alert("Invalid temperature, try again");
+        return;
+    }
+    
+    // use the worker function and get the output
+    var sum = tempConvertFah(fahrenheit);
+    
+    // do something "useful" - i.e., display the answer
+    document.getElementById("outputAreaCelsius").innerHTML = sum + " degrees Celsius";
+
+}
+
+
+/**
+ * keystronke
+ **/
+function check() {
+    if (window.even.which == 13) {
+        tempConvertCelCallback();
+    }
+}
