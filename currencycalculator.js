ff886fce2ade8e29eeb09186e2f4e015ffd87556
Kazim Agirnas <aagirnas@gmail.com>
2014-08-21 16:49:40 -0600

    updated Baht spelling and refractured both files

--- currencycalculator.js
+++ currencycalculator.js
@@ -1,21 +1,21 @@
 
-    function convertEuroToBath() {
-         var first = document.getElementById("first").value;
+function convertEuroToBath() {
+     var first = document.getElementById("first").value;
    
-    // sanitize inputs 
-    first = parseFloat(first);
-     //verify since these could potentially be NaN
-    if(isNaN(first) === true) {
+          // sanitize inputs 
+     first = parseFloat(first);
+          //verify since these could potentially be NaN
+     if(isNaN(first) === true) {
         alert ("Invalid inputs detected"); // this verification procedure is called a fail safe
-        return; //two bit implications of return - it returns a value (in this case nothing)and second it ends the function.
-    }
+     return; //two bit implications of return - it returns a value (in this case nothing)and second it ends the function.
+}
     
 
-    //take product calculation and calculate var product
+     //take product calculation and calculate var product
 
-    var product = (first * 42.4665);
+     var product = (first * 42.4665);
 
-    //When the task is complete display the results
+     //When the task is complete display the results
 
      document.getElementById("outputArea").innerHTML = "THB = " + product;
 }
